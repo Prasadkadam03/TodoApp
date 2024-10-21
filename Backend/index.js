d0458b62fcbe6ac79212ff5a776556f3f0d1d06a
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-10-21 20:59:59 +0530

    Update backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,33 @@
 // basic express boiler plate code 
 
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
-const types = require("types.js")
 
 app.use(express.json());
 app.post("/todo", (req , res) => {
+    const  createPayLoad = req.body;
+    const parsePayLoad = createTodo.safeParse(createPayLoad);
+    if(!parsePayLoad.success ) {
+        res.status(411).json({
+            msg : "You sent a wrong input"
+        })
+        return;
+    }
 
 })
+
 app.get("/todo", (req , res) => {
-    
+   
 })
+
 app.put("/completed", (req , res) => {
-    
+    const  updatePayLoad = req.body;
+    const parsePayLoad = updateTodo.safeParse(updatePayLoad);
+    if(!parsePayLoad.success) {
+        res.status(411).json({
+            msg : "You sent a wrong input"
+        })
+        return;
+    }
 })
