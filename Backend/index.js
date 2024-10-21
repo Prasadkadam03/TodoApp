61f101edde14734abe3baed0925927503457e792
Prasadkadam03 <prasadkadam29503@gmail.com>
2024-10-22 01:15:50 +0530

    Complete backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,12 @@
-// basic express boiler plate code 
+// basic express boiler plate code
 
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 const app = express();
 
 app.use(express.json());
-app.post("/todo", (req , res) => {
+app.post("/todo", async (req , res) => {
     const  createPayLoad = req.body;
     const parsePayLoad = createTodo.safeParse(createPayLoad);
     if(!parsePayLoad.success ) {
@@ -15,13 +16,28 @@ app.post("/todo", (req , res) => {
         return;
     }
 
+    await todo.create({
+        title : createPayLoad.title,
+        description : createPayLoad.description,
+        completed: false,
+    })
+    res.json({
+        msg : "todo created successfully"
+    })
+
 })
 
-app.get("/todo", (req , res) => {
-   
+app.get("/todo", async (req , res) => {
+   const todos = await todo.find({});
+   console.log(todos);
+
+   res.json({
+    todos
+   })
+
 })
 
-app.put("/completed", (req , res) => {
+app.put("/completed",async (req , res) => {
     const  updatePayLoad = req.body;
     const parsePayLoad = updateTodo.safeParse(updatePayLoad);
     if(!parsePayLoad.success) {
@@ -30,4 +46,17 @@ app.put("/completed", (req , res) => {
         })
         return;
     }
+
+    await todo.udate ({
+        _id: req.body._id
+    },{
+        completed: true
+    })
+    res.json({
+        msg:"Todo mafrk as completed"
+    })
+
 })
+
+
+app.listen(3000);
